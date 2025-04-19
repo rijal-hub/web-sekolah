3a1de9ec695b97d5c400eedbb3e868a1ff5a4b08
rijal-hub <hklrjld@gmail.com>
2025-04-19 23:19:24 +0700

    .

--- index.php
+++ index.php
@@ -260,7 +260,8 @@ if ($result_kontak->num_rows > 0) {
             </ul>
           </li>
           <li><a href="contact.php">Kontak</a></li>
-          <li><a href="login.php" class="login-box">Login</a></li>
+          <li id="admin-login-btn" style="display: none;">
+          <a href="login.php" class="login-box">Login</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
@@ -439,6 +440,13 @@ if ($result_kontak->num_rows > 0) {
   <!-- Main JS File -->
   <script src="assets/js/main.js"></script>
 
+  <script>
+  document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key.toLowerCase() === 'l') {
+      document.getElementById('admin-login-btn').style.display = 'block';
+    }
+  });
+  </script>
 </body>
 
 </html>
