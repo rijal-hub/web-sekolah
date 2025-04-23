5097b16ad7e1259e8dd1ee23767321c0e1906d07
rijal-hub <hklrjld@gmail.com>
2025-04-23 10:38:58 +0700

    .

--- login.php
+++ login.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="assets/css/login.css">z
+    <link rel="stylesheet" href="assets/css/login.css">
 </head>
 <body>
 <a href="index.php" class="back-button">
